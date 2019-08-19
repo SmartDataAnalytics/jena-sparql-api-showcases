7c315e15605ffccbdb584ff93dac81da5e2601f3
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-08-19 18:06:53 +0200

    Updated example

--- jena-sparql-api-showcase-proxy/src/test/java/org/aksw/jena_sparql_api_showcase/proxy/TestDynamicRDFNodeViews.java
+++ jena-sparql-api-showcase-proxy/src/test/java/org/aksw/jena_sparql_api_showcase/proxy/TestDynamicRDFNodeViews.java
@@ -2,23 +2,14 @@ package org.aksw.jena_sparql_api_showcase.proxy;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
-import org.aksw.commons.accessors.CollectionFromConverter;
-import org.aksw.commons.collections.sets.SetFromCollection;
 import org.aksw.jena_sparql_api.mapper.annotation.IriNs;
+import org.aksw.jena_sparql_api.mapper.annotation.PolymorphicOnly;
 import org.aksw.jena_sparql_api.mapper.annotation.RdfType;
 import org.aksw.jena_sparql_api.mapper.annotation.ResourceView;
 import org.aksw.jena_sparql_api.mapper.proxy.JenaPluginUtils;
-import org.aksw.jena_sparql_api.mapper.proxy.TypeDecider;
-import org.aksw.jena_sparql_api.rdf.collections.ConverterFromRDFNodeMapper;
-import org.aksw.jena_sparql_api.rdf.collections.RDFNodeMapper;
-import org.aksw.jena_sparql_api.rdf.collections.RDFNodeMapperImpl;
 import org.aksw.jena_sparql_api.rdf.collections.ResourceUtils;
-import org.aksw.jena_sparql_api.rdf.collections.SetFromPropertyValues;
-import org.apache.jena.datatypes.TypeMapper;
 import org.apache.jena.enhanced.EnhGraph;
 import org.apache.jena.graph.Node;
 import org.apache.jena.rdf.model.Model;
@@ -29,12 +20,8 @@ import org.apache.jena.rdf.model.impl.ResourceImpl;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.vocabulary.RDF;
-import org.apache.jena.vocabulary.RDFS;
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.github.jsonldjava.shaded.com.google.common.collect.Lists;
-import com.google.common.base.Converter;
 
 
 /**
@@ -100,6 +87,9 @@ public class TestDynamicRDFNodeViews {
 
 	
 	/**
+	 * Example of using an abstract class instead of
+	 * an interface. ResourceImpl must be a base class,
+	 * and the constructor for (Node, EnhGraph) must be defined.
 	 * 
 	 * @author raven
 	 *
@@ -118,68 +108,19 @@ public class TestDynamicRDFNodeViews {
 	    public abstract void setBiggestVessel(Vessel vessel);
 	    
 		@IriNs("eg")
+		// PolymoprhicOnly: Yield a collection that only exposes those resources that are known subclasses
+		// of Vessel - no views are requested or returned for resources with other or unknown types. 
+		// Example: With @PolymorphicOnly, getVessel(Motorboat.class) will only return known instances of Motorboat and its subclasses.
+		//          Without @PolymorphicOnly, getVessel(Motorboat.class) will in addition return any other resource using a Motorboat view.
+		@PolymorphicOnly
 		public abstract <T extends Vessel> Collection<T> getVessels(Class<T> clazz);
 
 		// Test case for setting/getting an arbitrary object
 	    @IriNs("eg")
 	    public abstract void setObject(Object o);
 	    public abstract Object getObject();
-
-		//@IriNs("eg")
-//		public <T extends Vessel> Collection<T> getVessels(Class<T> clazz) {
-//			//TypeDecider typeDecider = new TypeDeciderImpl().registerClasses(classes);
-//			TypeDecider typeDecider = JenaPluginUtils.getTypeDecider();
-//			
-//			Collection<T> it = CollectionFromIterable.create(() -> ResourceUtils.listPropertyValues(this, p, RDFNodeMappers.from(clazz, typeDecider)));
-//			return it;
-//		}
 	}
 
-	@Test
-	public void testTypeDecider() {
-		
-		List<Integer> b = Lists.newArrayList(1, 1, 2, 2, 2, 3, 3, 3);
-		Set<Integer> test = new SetFromCollection<>(b);
-		System.out.println(test + " " + test.size() + " " + b);
-		
-		test.remove(1);
-		System.out.println(test + " " + test.size() + " " + b);
-
-		Iterator<Integer> it = test.iterator();
-		it.next();
-		it.remove();
-		System.out.println(test + " " + test.size() + " " + b);
-		
-		
-		JenaPluginUtils.registerResourceClasses(classes);
-		
-//		TypeDecider typeDecider = new TypeDeciderImpl().scan(TestDynamicViews.class);
-		
-		Model m = ModelFactory.createDefaultModel();
-		
-		Resource root = m.createResource()
-			.addLiteral(RDFS.label, 1)
-			.addLiteral(RDFS.label, 2.0)
-			.addProperty(RDFS.label, m.createResource("x:mb1").as(Motorboat.class))
-			.addProperty(RDFS.label, m.createResource("x:sb1").as(Sailboat.class))
-			.addProperty(RDFS.label, m.createResource("x:sy1").as(SailingYacht.class));
-
-		TypeMapper typeMapper = TypeMapper.getInstance();
-		TypeDecider typeDecider = JenaPluginUtils.getTypeDecider();
-		
-		RDFNodeMapper<?> mapper = new RDFNodeMapperImpl<>(Long.class, typeMapper, typeDecider);
-
-		Collection<RDFNode> backend = new SetFromPropertyValues<RDFNode>(root, RDFS.label, RDFNode.class);
-		
-		
-		Converter<RDFNode, ?> converter = new ConverterFromRDFNodeMapper<>(mapper);
-		Collection<?> col = new CollectionFromConverter<>(backend, converter);
-
-		for(Object o : col) {
-			System.out.println(o + " -> " + o.getClass());
-		}
-	}
-	
 	@Test
 	public void testDynamicSetViewsWithTypes() {
 		// Register the view classes
